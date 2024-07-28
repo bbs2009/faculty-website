bbfaea72d88c8eae0f5e5199e8adb16475721688
bbs2009 <burlakabogdan@gmail.com>
2024-07-28 09:53:20 +0300

    fix load articles

--- src/app/ui/components/Articles/Articles.js
+++ src/app/ui/components/Articles/Articles.js
@@ -10,35 +10,35 @@ import ButtonTag from '../ButtonTag/ButtonTag';
 import { useGetArticles, useSearchArticles } from '../../../hooks/useAPI';
 import ArticleDetailBack from '../ArticleDetailBack/ArticleDetailBack';
 
-function useArticles(pageType, page, PAGE_SIZE, searchString) {
-  if (pageType === 'default') {
-    return useGetArticles(page, PAGE_SIZE);
-  } else if (pageType === 'search') {
-    return useSearchArticles(searchString, page, PAGE_SIZE);
-  }
-}
+// function useArticles(pageType, page, PAGE_SIZE, searchString) {
+//   if (pageType === 'default') {
+//     return useGetArticles(page, PAGE_SIZE);
+//   } else if (pageType === 'search') {
+//     return useSearchArticles(searchString, page, PAGE_SIZE);
+//   }
+// }
 
-export default function Articles({ className, pageTitle, pageType = 'default', searchString, ...props }) {
+export default function Articles({ className, ...props }) {
   const router = useRouter();
   const PAGE_SIZE = 6;
   const [page, setPage] = useState(1);
   const [publications, setPublications] = useState([]);
-  const [searchStringState, setSearchStringState] = useState('');
-  const [searchArticleCount, setSearchArticleCount] = useState(0);
+  // const [searchStringState, setSearchStringState] = useState('');
+  // const [searchArticleCount, setSearchArticleCount] = useState(0);
 
-  const { articles, isValidating, isLoading, totalElements } = useArticles(pageType, page, PAGE_SIZE, searchString);
+  const { articles, isValidating, isLoading } = useGetArticles(page, PAGE_SIZE);
 
   useEffect(() => {
-    if (searchString) {
-      setSearchStringState(searchString);
-      setSearchArticleCount(totalElements);
-      setPublications([]);
-    }
+    // if (searchString) {
+    //   setSearchStringState(searchString);
+    //   setSearchArticleCount(totalElements);
+    //   setPublications([]);
+    // }
 
     if (articles) {
       setPublications((prevPublications) => [...prevPublications, ...articles]);
     }
-  }, [articles, searchString, totalElements]);
+  }, [articles]);
 
   const isLoadingMore = isLoading || (page > 0 && publications && typeof publications[page - 1] === 'undefined');
   const isEmpty = publications && publications[0] && publications[0].length === 0;
@@ -46,8 +46,8 @@ export default function Articles({ className, pageTitle, pageType = 'default', s
 
   return (
     <div className={clsx(className, styles.articles_list)} {...props}>
-      {pageType === 'default' ? <h1 className={styles.h1_main}>Наукова робота:</h1> : null}
-      {pageType === 'search' ? <SearchResults searchStringState={searchStringState} searchArticleCount={searchArticleCount} /> : null}
+      <h1 className={styles.h1_main}>Наукова робота:</h1>
+      {/* {pageType === 'search' ? <SearchResults searchStringState={searchStringState} searchArticleCount={searchArticleCount} /> : null} */}
 
       {isEmpty ? <p>Статті відсутні</p> : null}
 
@@ -79,15 +79,15 @@ export default function Articles({ className, pageTitle, pageType = 'default', s
   );
 }
 
-function SearchResults({ searchStringState, searchArticleCount }) {
-  return (
-    <>
-      <ArticleDetailBack />
-      <h1 className={styles.h1_search}>Результати пошуку:</h1>
-      <span className={styles.search_result}>
-        За пошуковим запитом <span className={styles.search_result_bold}>{searchStringState}</span> було знайдено
-        <span className={styles.search_result_bold}> {searchArticleCount} статей</span>
-      </span>
-    </>
-  );
-}
+// function SearchResults({ searchStringState, searchArticleCount }) {
+//   return (
+//     <>
+//       <ArticleDetailBack />
+//       <h1 className={styles.h1_search}>Результати пошуку:</h1>
+//       <span className={styles.search_result}>
+//         За пошуковим запитом <span className={styles.search_result_bold}>{searchStringState}</span> було знайдено
+//         <span className={styles.search_result_bold}> {searchArticleCount} статей</span>
+//       </span>
+//     </>
+//   );
+// }
